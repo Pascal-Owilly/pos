b5cd604a38ba6527b2d99e4d188afc5d402d4771
Pascal Owilly <owillypascal@gmail.com>
2024-05-18 10:53:17 +0300

    initited successfully

---
+++ django_pos/products/models.py
@@ -0,0 +1,118 @@
+from django.db import models
+from django.forms import model_to_dict
+
+
+class Category(models.Model):
+    STATUS_CHOICES = (  # new
+        ("ACTIVE", "Active"),
+        ("INACTIVE", "Inactive")
+    )
+
+    name = models.CharField(max_length=256)
+    description = models.TextField(max_length=256)
+    status = models.CharField(
+        choices=STATUS_CHOICES,
+        max_length=100,
+        verbose_name="Status of the category",
+    )
+
+    class Meta:
+        # Table's name
+        db_table = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    STATUS_CHOICES = (  # new
+        ("ACTIVE", "Active"),
+        ("INACTIVE", "Inactive")
+    )
+
+    name = models.CharField(max_length=256)
+    description = models.TextField(max_length=256)
+    status = models.CharField(
+        choices=STATUS_CHOICES,
+        max_length=100,
+        verbose_name="Status of the product",
+    )
+    category = models.ForeignKey(
+        Category, related_name="category", on_delete=models.CASCADE, db_column='category')
+
+    price = models.FloatField(default=0)
+
+    class Meta:
+        # Table's name
+        db_table = "Product"
+
+    def __str__(self) -> str:
+        return self.name
+
+    def to_json(self):
+        item = model_to_dict(self)
+        item['id'] = self.id
+        item['text'] = self.name
+        item['category'] = self.category.name
+        item['quantity'] = 1
+        item['total_product'] = 0
+        return item
+
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    contact = models.CharField(max_length=255, null=True, blank=True)
+    products = models.ManyToManyField(Product, through='StoreInventory', related_name='stores')
+    product_quantities = models.TextField(default="{}")  
+
+    date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+    def update_product_quantity(self, product_id, quantity):
+        """
+        Update the quantity of a product in the store
+        """
+        # Parse the existing product quantities JSON string
+        try:
+            product_quantities = json.loads(self.product_quantities)
+        except json.JSONDecodeError:
+            product_quantities = {}
+
+        # Update the quantity of the specified product
+        product_quantities[str(product_id)] = quantity
+
+        # Save the updated product quantities back to the model
+        self.product_quantities = json.dumps(product_quantities, ensure_ascii=False)
+        self.save()
+
+        print(f"Updated product quantity for product ID {product_id} in store {self.name} to {quantity}.")
+
+class StoreInventory(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('store', 'product')
+
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=255)
+    contact_info = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Purchase(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product.name} at {self.store.name} ({self.quantity})'
+
---
+++ django_pos/products/urls.py
@@ -0,0 +1,48 @@
+from django.urls import path
+
+from . import views
+from sales.views import daily_reports, product_report
+app_name = "products"
+
+urlpatterns = [
+    # List categories
+    path('categories', views.CategoriesListView, name='categories_list'),
+    # Add category
+    path('categories/add', views.CategoriesAddView, name='categories_add'),
+    # Update category
+    path('categories/update/<str:category_id>',
+         views.CategoriesUpdateView, name='categories_update'),
+    # Delete category
+    path('categories/delete/<str:category_id>',
+         views.CategoriesDeleteView, name='categories_delete'),
+
+    # List products
+    path('', views.ProductsListView, name='products_list'),
+    # Add product
+    path('add', views.ProductsAddView, name='products_add'),
+    # Update product
+    path('update/<str:product_id>',
+         views.ProductsUpdateView, name='products_update'),
+    # Delete product
+    path('delete/<str:product_id>',
+         views.ProductsDeleteView, name='products_delete'),
+    # Get products AJAX
+    path("get", views.GetProductsAJAXView, name="get_products"),
+
+    # Add products
+    path('add_purchase/', views.add_purchase, name='add_purchase'),  # Make sure to have a trailing slash here
+
+    # List purchases
+    path('purchases/', views.purchase_list, name='purchase_list'),  
+    path('stores/', views.store_list, name='store_list'),  
+
+    path('stores/add/', views.add_store, name='add_store'),
+    path('vendors/add/', views.add_vendor, name='add_vendor'),
+    path('product-reports/', product_report, name='product_reports'),
+    path('reports/', views.reports, name='reports'), 
+    path('employee_reports/', views.employee_reports, name='employee_reports'),
+    path('sales_reports/', daily_reports, name='daily_reports'),
+    path('bp_vs_sl_reports/', views.bp_vs_sl_reports, name='bp_vs_sl_reports'),
+    path('wastage_reports/', views.wastage_reports, name='wastage_reports'),
+
+]
